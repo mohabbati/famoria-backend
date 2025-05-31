710a0939912dfa06f9beb9973c87f6e500467823
Mohammad Mohabbati <Mohammad.Mohabbati@rhenus.com>
2025-05-31 13:32:24 +0200

    Add Azure services and project references
    Updated `Famoria.AppHost.csproj` to include new package references for Azure services and a community toolkit. Added project references for various worker projects.
    
    Modified `Program.cs` to set up a distributed application with Azure Cosmos DB and Azure Storage, configuring databases and containers, and ensuring projects wait for resources before running.

--- aspire/Famoria.AppHost/Program.cs
+++ aspire/Famoria.AppHost/Program.cs
@@ -1,3 +1,40 @@
+#pragma warning disable ASPIRECOSMOSDB001
+
 var builder = DistributedApplication.CreateBuilder(args);
+
+var cosmos = builder.AddAzureCosmosDB("cosmos").RunAsPreviewEmulator();
+var cosmosDb = cosmos.AddCosmosDatabase("famoria-db", "famoria");
+cosmosDb.AddContainer("families", "/id");
+cosmosDb.AddContainer("family-items", "/FamilyId");
+cosmosDb.AddContainer("family-tasks", "/FamilyId");
+
+var storage = builder.AddAzureStorage("storage").RunAsEmulator()
+    .AddBlobs("blobs")
+    .AddBlobContainer("emails");
+
+builder.AddProject<Projects.Famoria_Api>("famoria-api")
+    .WithReference(cosmos)
+    .WaitFor(cosmosDb);
+builder.AddProject<Projects.Famoria_Email_Fetcher_Worker>("famoria-email-fetcher-worker")
+    .WithReference(cosmos)
+    .WaitFor(cosmosDb)
+    .WithReference(storage)
+    .WaitFor(storage);
+builder.AddProject<Projects.Famoria_Email_Filter_Worker>("famoria-email-filter-worker")
+    .WithReference(cosmos)
+    .WaitFor(cosmosDb)
+    .WithReference(storage)
+    .WaitFor(storage);
+builder.AddProject<Projects.Famoria_Tasker_Worker>("famoria-tasker-worker")
+    .WithReference(cosmos)
+    .WaitFor(cosmosDb)
+    .WithReference(storage)
+    .WaitFor(storage);
+builder.AddProject<Projects.Famoria_Summarizer_Worker>("famoria-summarizer-worker")
+    .WithReference(cosmos)
+    .WaitFor(cosmosDb)
+    .WithReference(storage)
+    .WaitFor(storage);
+
 
 builder.Build().Run();
