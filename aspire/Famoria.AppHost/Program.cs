d4c7f43b3d59bb2f358a08a4ddb25ff9e655de35
Mohammad Mohabbati <Mohammad.Mohabbati@rhenus.com>
2025-05-31 21:17:13 +0200

    Add unit and functional tests; update Azure configurations
    - Updated `Famoria.sln` to include `Famoria.Unit.Tests` and `Famoria.Functional.Tests` projects with necessary build configurations.
    - Added `CommunityToolkit.Aspire.Hosting.MailPit` package reference in `Famoria.AppHost.csproj`.
    - Refined Azure Cosmos DB and Azure Storage configurations in `Program.cs`, including a new setup for Cosmos DB and updated blob container references.

--- aspire/Famoria.AppHost/Program.cs
+++ aspire/Famoria.AppHost/Program.cs
@@ -2,39 +2,56 @@
 
 var builder = DistributedApplication.CreateBuilder(args);
 
-var cosmos = builder.AddAzureCosmosDB("cosmos").RunAsPreviewEmulator();
-var cosmosDb = cosmos.AddCosmosDatabase("famoria-db", "famoria");
+var cosmos = builder.AddAzureCosmosDB("cosmos").RunAsEmulator(x =>
+    {
+        x.WithLifetime(ContainerLifetime.Persistent);
+        x.WithUrlForEndpoint("https", u =>
+        {
+            x.Resource.TryGetAnnotationsOfType<EndpointAnnotation>(out var annotations);
+            var httpsAnnotation = annotations!.First(z => z.Name == "https");
+            u.Url = $"{httpsAnnotation.UriScheme}://{httpsAnnotation.TargetHost}:{httpsAnnotation.TargetPort}/_explorer/index.html";
+            u.DisplayText = "Data Explorer UI";
+        });
+    })
+    .PublishAsConnectionString();
+var cosmosDb = cosmos.AddCosmosDatabase("cosmos-db", "famoria");
 cosmosDb.AddContainer("families", "/id");
 cosmosDb.AddContainer("family-items", "/FamilyId");
 cosmosDb.AddContainer("family-tasks", "/FamilyId");
 
-var storage = builder.AddAzureStorage("storage").RunAsEmulator()
+var blobContainer = builder.AddAzureStorage("blob-container").RunAsEmulator(x =>
+    {
+        x.WithLifetime(ContainerLifetime.Persistent);
+    })
     .AddBlobs("blobs")
-    .AddBlobContainer("emails");
+    .AddBlobContainer("famoria");
+
+var mailpit = builder.AddMailPit("mail-pit").WithLifetime(ContainerLifetime.Persistent);
 
 builder.AddProject<Projects.Famoria_Api>("famoria-api")
     .WithReference(cosmos)
     .WaitFor(cosmosDb);
 builder.AddProject<Projects.Famoria_Email_Fetcher_Worker>("famoria-email-fetcher-worker")
+    .WithReference(mailpit)
+    .WaitFor(mailpit)
     .WithReference(cosmos)
     .WaitFor(cosmosDb)
-    .WithReference(storage)
-    .WaitFor(storage);
+    .WithReference(blobContainer)
+    .WaitFor(blobContainer);
 builder.AddProject<Projects.Famoria_Email_Filter_Worker>("famoria-email-filter-worker")
     .WithReference(cosmos)
     .WaitFor(cosmosDb)
-    .WithReference(storage)
-    .WaitFor(storage);
+    .WithReference(blobContainer)
+    .WaitFor(blobContainer);
 builder.AddProject<Projects.Famoria_Tasker_Worker>("famoria-tasker-worker")
     .WithReference(cosmos)
     .WaitFor(cosmosDb)
-    .WithReference(storage)
-    .WaitFor(storage);
+    .WithReference(blobContainer)
+    .WaitFor(blobContainer);
 builder.AddProject<Projects.Famoria_Summarizer_Worker>("famoria-summarizer-worker")
     .WithReference(cosmos)
     .WaitFor(cosmosDb)
-    .WithReference(storage)
-    .WaitFor(storage);
-
+    .WithReference(blobContainer)
+    .WaitFor(blobContainer);
 
 builder.Build().Run();
