0943d26728acbce8ef124067d168b48783289d39
mohabbati <mohabbati@live.com>
2025-06-05 16:08:32 +0200

    Add Swagger with basic configuration

--- src/Famoria.Api/Program.cs
+++ src/Famoria.Api/Program.cs
@@ -15,8 +15,11 @@ builder
     .AddApiServices();
 
 builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(options =>
+{
+    options.SwaggerDoc("v1", new() { Title = "Famoria API", Version = "v1" });
+});
 
 builder
     .AddInfrastructure(builder.Configuration.Get<AppSettings>()!)
@@ -29,7 +32,12 @@ app.MapDefaultEndpoints();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI(options =>
+    {
+        options.SwaggerEndpoint("/swagger/v1/swagger.json", "Famoria API V1");
+        options.RoutePrefix = string.Empty;
+    });
 }
 
 app.UseHttpsRedirection();
