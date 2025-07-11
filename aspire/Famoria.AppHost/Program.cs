3cc2a4e3dbfb454351f7c2b8a6386100e5f79c44
Mohammad Mohabbati <Mohammad.Mohabbati@rhenus.com>
2025-07-11 17:23:17 +0200

    Add Ollama service and new Cosmos DB container
    - Introduced a new Cosmos DB container for "family-items-audits".
    - Added Ollama service with specific configurations.
    - Updated Famoria Summarizer Worker to reference and wait for the Ollama service.
    - Included a commented-out section for the Famoria AuthTester project.

--- aspire/Famoria.AppHost/Program.cs
+++ aspire/Famoria.AppHost/Program.cs
@@ -7,14 +7,14 @@ var cosmos = builder.AddAzureCosmosDB("cosmos").RunAsEmulator(x =>
         x.WithGatewayPort(8081);
         x.WithLifetime(ContainerLifetime.Persistent);
         x.WithUrl("https://localhost:8081/_explorer/index.html", "Data Explorer UI");
-    })
-    .PublishAsConnectionString();
+    });
 var cosmosDb = cosmos.AddCosmosDatabase("cosmos-db", "famoria");
 cosmosDb.AddContainer("users", "/id");
 cosmosDb.AddContainer("families", "/id");
 cosmosDb.AddContainer("user-linked-accounts", "/provider");
 cosmosDb.AddContainer("family-items", "/familyId");
 cosmosDb.AddContainer("family-items-leases", "/id");
+cosmosDb.AddContainer("family-items-audits", "/id");
 
 var storageAccount = builder.AddAzureStorage("storage-account").RunAsEmulator(x =>
     {
@@ -24,6 +24,11 @@ var storageAccount = builder.AddAzureStorage("storage-account").RunAsEmulator(x 
     });
 var blobs = storageAccount.AddBlobs("blobs");
 var blobContainer = blobs.AddBlobContainer("blob-container", "famoria");
+
+var ollama = builder.AddOllama("ollama", 11434)
+    .WithLifetime(ContainerLifetime.Persistent)
+    .WithDataVolume("ollama")
+    .AddModel("llama3");
 
 var api = builder.AddProject<Projects.Famoria_Api>("famoria-api")
     .WithEnvironment("CosmosDbSettings:AccountEndpoint", "https://a.valid.url/")
@@ -48,9 +53,15 @@ builder.AddProject<Projects.Famoria_Summarizer_Worker>("famoria-summarizer-worke
     .WithEnvironment("CosmosDbSettings:DatabaseId", "famoria")
     .WithEnvironment("BlobContainerSettings:ServiceUri", "https://a.valid.url/")
     .WithEnvironment("BlobContainerSettings:ContainerName", "famoria")
+    .WithEnvironment("Ai:Endpoint", "http://localhost:11434/")
     .WithReference(cosmos)
     .WaitFor(cosmos)
     .WithReference(blobs)
-    .WaitFor(blobs);
+    .WaitFor(blobs)
+    .WithReference(ollama)
+    .WaitFor(ollama);
+
+//builder.AddProject<Projects.Famoria_AuthTester>("famoria-auth-tester")
+//    .WaitFor(api);
 
 builder.Build().Run();
